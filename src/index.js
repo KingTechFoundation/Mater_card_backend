435c76028007787933a7c2344120e5f2609f8c5d
KingTechFoundation <muhirehillary459@gmail.com>
2024-12-25 21:50:28 +0300

    removed redirect

--- src/index.js
+++ src/index.js
@@ -43,15 +43,6 @@ app.use(
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 
-// Serve static files in production
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/dist')));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'));
-  });
-}
-
 // Start server
 server.listen(PORT, () => {
   console.log('server is running on PORT:' + PORT);
